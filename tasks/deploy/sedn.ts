e701294bd310ada899abdb97ab0803ec7cc43b36
Derek <alexanderderekrein@gmail.com>
2022-10-23 10:02:34 -0400

    feat: enable payment

--- tasks/deploy/sedn.ts
+++ tasks/deploy/sedn.ts
@@ -9,12 +9,12 @@ function timeout(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const usdPriceFeeds: any = {
-  'hardhat': '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-  'mainnet': '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-  'rinkeby': '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-  'polygon-mainnet': '0xAB594600376Ec9fD91F8e885dADF0CE036862dE0',
-  'polygon-mumbai': '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada',
+const usdcTokenAddressess: any = {
+  'hardhat': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  'mainnet': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  'rinkeby': '0xeb8f08a975ab53e34d8a0330e0d34de942c95926',
+  'polygon-mainnet': '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+  'polygon-mumbai': '0xe6b8a5cf854791412c1f6efc7caf629f5df1c747',
 };
 
 task("deploy:Sedn").setAction(async function (taskArguments: TaskArguments, { ethers, run, network }) {
@@ -22,7 +22,7 @@ task("deploy:Sedn").setAction(async function (taskArguments: TaskArguments, { et
   const sednFactory: Sedn__factory = <Sedn__factory>(
     await ethers.getContractFactory("Sedn")
   );
-  const sedn: Sedn = <Sedn>await sednFactory.connect(signers[0]).deploy(usdPriceFeeds[network.name]);
+  const sedn: Sedn = <Sedn>await sednFactory.connect(signers[0]).deploy(usdcTokenAddressess[network.name]);
   await sedn.deployed();
   console.log("Sedn deployed to: ", sedn.address);
 
@@ -32,7 +32,7 @@ task("deploy:Sedn").setAction(async function (taskArguments: TaskArguments, { et
     await run("verify:verify", {
       address: sedn.address,
       network: network.name,
-      constructorArguments: [usdPriceFeeds[network.name]]
+      constructorArguments: [usdcTokenAddressess[network.name]]
     });
   }
 });
---
+++ test/sedn/Sedn.ts
@@ -0,0 +1,48 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import type { Sedn } from "../../src/types/Sedn";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers } from "hardhat";
+
+
+describe("Sedn Contract", function () {
+  async function deploySednFixture() {
+    const mockUSDCFactory = await ethers.getContractFactory("MockUSDC");
+    const sednFactory = await ethers.getContractFactory("Sedn");
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const usdc = await mockUSDCFactory.connect(owner).deploy("USD Coin", "USDC");
+    const mintToken = await usdc.mint(owner.address, ethers.utils.parseEther("2048"));
+    const sedn = await sednFactory.connect(owner).deploy(usdc.address);
+
+    return { sedn, usdc, owner, addr1, addr2 };
+  }
+  describe("Switch Creation", function () {
+    let sedn: Sedn;
+    let owner: SignerWithAddress;
+    let usdc: Contract;
+    let addr1: SignerWithAddress;
+    beforeEach(async function () {
+      const deployed = await loadFixture(deploySednFixture);
+      sedn = deployed.sedn;
+      owner = deployed.owner;
+      usdc = deployed.usdc;
+      addr1 = deployed.addr1;
+    });
+    it("send funds to an unregistered user", async function () {
+      await usdc.approve(sedn.address, 10);
+      await sedn.connect(owner).sednToUnregistered(10, "hello", "world");
+      expect(await usdc.balanceOf(sedn.address)).to.equal(10);
+      const beforeClaim = await usdc.balanceOf(owner.address);
+      await sedn.connect(owner).claim("hello", "world");
+      const afterClaim = await usdc.balanceOf(owner.address);
+      expect(afterClaim.sub(beforeClaim)).to.equal(10);
+    });
+    it("send funds to registered user", async function () {
+      await sedn.connect(owner).setPreferredAddress(addr1.address, "+14157588102");
+      await usdc.approve(sedn.address, 10);
+      await sedn.connect(owner).sednToRegistered(10, "hello", "world", "+14157588102");
+      expect(await usdc.balanceOf(addr1.address)).to.equal(10);
+    });
+  });
+});
