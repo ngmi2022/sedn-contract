01f0b2799d62dc3066a59c6d2a0f31995c557cda
ferdi/friedefuerst <fe@pleb.gmbh>
2022-10-26 15:37:03 +0200

    feat: implements bridged claims

--- contracts/Sedn.sol
+++ contracts/Sedn.sol
@@ -9,8 +9,59 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 error SednError();
 
-contract Sedn is Ownable {
+interface IUserRequest {
+    /**
+    // @param id route id of middleware to be used
+    // @param optionalNativeAmount is the amount of native asset that the route requires 
+    // @param inputToken token address which will be swapped to
+    // BridgeRequest inputToken 
+    // @param data to be used by middleware
+    */
+    struct MiddlewareRequest {
+        uint256 id;
+        uint256 optionalNativeAmount;
+        address inputToken;
+        bytes data;
+    }
+
+    /**
+    // @param id route id of bridge to be used
+    // @param optionalNativeAmount optinal native amount, to be used
+    // when bridge needs native token along with ERC20    
+    // @param inputToken token addresss which will be bridged 
+    // @param data bridgeData to be used by bridge
+    */
+    struct BridgeRequest {
+        uint256 id;
+        uint256 optionalNativeAmount;
+        address inputToken;
+        bytes data;
+    }
+
+    /**
+    // @param receiverAddress Recipient address to recieve funds on destination chain
+    // @param toChainId Destination ChainId
+    // @param amount amount to be swapped if middlewareId is 0  it will be
+    // the amount to be bridged
+    // @param middlewareRequest middleware Requestdata
+    // @param bridgeRequest bridge request data
+    */
+    struct UserRequest {
+        address receiverAddress;
+        uint256 toChainId;
+        uint256 amount;
+        MiddlewareRequest middlewareRequest;
+        BridgeRequest bridgeRequest;
+    }
+}
+
+interface IRegistry is IUserRequest {
+    function outboundTransferTo(UserRequest calldata _userRequest) external payable;
+}
+
+contract Sedn is Ownable, IUserRequest {
     IERC20 public usdcToken;
+    IRegistry public registry;
     uint256 public paymentCounter;
     address public addressDelegate;
 
@@ -18,7 +69,7 @@ contract Sedn is Ownable {
 
     struct Payment {
         address from;
-        uint amount;
+        uint256 amount;
         bool completed;
     }
 
@@ -26,13 +77,22 @@ contract Sedn is Ownable {
     mapping(bytes32 => Payment) private payments;
     mapping(bytes32 => address) private preferredAddresses;
 
-    constructor(address _usdcTokenAddressForChain) {
-        console.log("Deploying the Sedn Contract; USDC Token Address:", _usdcTokenAddressForChain);
+    constructor(address _usdcTokenAddressForChain, address _registryDeploymentAddressForChain) {
+        console.log(
+            "Deploying the Sedn Contract; USDC Token Address: %s; Socket Registry: %s",
+            _usdcTokenAddressForChain,
+            _registryDeploymentAddressForChain
+        );
         usdcToken = IERC20(_usdcTokenAddressForChain);
+        registry = IRegistry(_registryDeploymentAddressForChain);
         setAddressDelegate(msg.sender);
     }
 
-    function sednToUnregistered(uint _amount, string calldata secret, string calldata nullifier) public {
+    function sednToUnregistered(
+        uint256 _amount,
+        string calldata secret,
+        string calldata nullifier
+    ) public {
         console.log("Attempting to send", _amount);
         require(_amount > 0, "amount should be > 0");
         bytes32 key = keccak256(abi.encodePacked(secret, nullifier));
@@ -43,7 +103,12 @@ contract Sedn is Ownable {
         console.log("Total payments", paymentCounter);
     }
 
-    function sednToRegistered(uint _amount, string calldata secret, string calldata nullifier, string calldata phone) public {
+    function sednToRegistered(
+        uint256 _amount,
+        string calldata secret,
+        string calldata nullifier,
+        string calldata phone
+    ) public {
         bytes32 _preferred = keccak256(abi.encodePacked(phone));
         require(preferredAddresses[_preferred] != address(0), "no preferred address found");
         address preferred = preferredAddresses[_preferred];
@@ -63,6 +128,21 @@ contract Sedn is Ownable {
         console.log("Claiming funds", payments[key].amount, msg.sender);
         usdcToken.approve(address(this), payments[key].amount);
         require(usdcToken.transferFrom(address(this), msg.sender, payments[key].amount), "transferFrom failed");
+        payments[key].completed = true;
+    }
+
+    function bridgeClaim(
+        string calldata secret,
+        string calldata nullifier,
+        UserRequest calldata _userRequest,
+        address bridgeImpl
+    ) public {
+        bytes32 key = keccak256(abi.encodePacked(secret, nullifier));
+        require(payments[key].from != address(0), "payment not found");
+        console.log("Bridge and claiming funds", payments[key].amount, msg.sender);
+        usdcToken.approve(address(registry), payments[key].amount);
+        usdcToken.approve(bridgeImpl, payments[key].amount);
+        registry.outboundTransferTo(_userRequest);
         payments[key].completed = true;
     }
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import { resolve } from "path";
 
 import "./tasks/accounts";
 import "./tasks/deploy";
+import "hardhat-etherscan-abi";
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
