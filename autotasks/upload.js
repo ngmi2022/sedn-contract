d4ebb60bc04826f0b2931035258531dad2cf4200
friedefürst <59935336+f-erdi1990@users.noreply.github.com>
2022-12-14 22:34:01 +0100

    feat: v2-init with working unit tests (#14)
    * feat: v2-init with working unit tests
    
    * feat: sedn testnet contract update
    
    * feat: add arbi-goerli integ test
    
    Co-authored-by: ferdi/friedefuerst <fe@pleb.gmbh>

--- autotasks/upload.js
+++ autotasks/upload.js
@@ -3,8 +3,8 @@ const { AutotaskClient } = require('defender-autotask-client');
 const ENVIRONMENT = process.env.ENVIRONMENT;
 
 const autotaskIds = {
-    'prod': ["507b3f04-18d3-41ab-9484-701a01fc2ffe", "8e4e19b7-0103-4552-ab68-3646966ab186", "ce515ed3-d267-4654-8843-e9fe7047c05d"],
-    'staging': ["ee577506-d647-4919-819e-bbe70e60f58c", "dba1d31c-cae3-4205-9786-5c2cf22c46af", "2d858f46-cc71-4628-af9f-efade0f6b1df"]
+    'prod': ["507b3f04-18d3-41ab-9484-701a01fc2ffe", "8e4e19b7-0103-4552-ab68-3646966ab186", "ce515ed3-d267-4654-8843-e9fe7047c05d", "0bfccce8-3489-411f-8fe2-38bb7e84104c", "eabbed25-d5bf-42d1-aa5e-e2a79760a071"],
+    'staging': ["ee577506-d647-4919-819e-bbe70e60f58c", "dba1d31c-cae3-4205-9786-5c2cf22c46af", "2d858f46-cc71-4628-af9f-efade0f6b1df", "f8d5a078-9408-4ab9-a390-8e94a83c53d2", "a123ebb6-4801-4a81-af03-7fd7d3b242a7"]
 };
 
 async function uploadCode(autotaskId, apiKey, apiSecret) {
